5b788fe2a080d47c39e4d5ad33d5a220f14d4fdb
Ashwin Ramesh <ashramesh1992@gmail.com>
2014-04-26 13:38:46 +1000

    added more db stuff

--- perceptron/database.py
+++ perceptron/database.py
@@ -48,89 +48,227 @@ def get_db_connection(db_name):
         return False
 
 
-"""
-Singular Database Create Table Functions
-"""
-
-
-def _create_perceptron_details_table(db_name):
+def _simple_query_wrapper(db_name, sql, args=None):
+    """
+    @description: wraps the database query in a nice function to stop code duplication
+    @TODO: make this so it returns data?
+    """  # TODO
     try:
         conn = get_db_connection(db_name)
         if not conn:
             return False
         c = conn.cursor()
 
+        if args:
+            c.execute(sql, args)
+        else:
+            c.execute(sql)
+
+        c.commit()
+        conn.close()
+        return True
+    except:
+        try:  # Incase that the connection was established and another error occured,
+              # close the connection before returning
+            conn.close()
+        except:
+            pass
+        return False
+
+"""
+Singular Database Create Table Functions
+    - table definitions can be found in sql/base_schema.sql
+"""
+
+
+def _create_perceptron_details_table(db_name):
         sql = """
             CREATE TABLE perceptron_details (
                 id INTEGER PRIMARY KEY,
                 iteratations INTEGER
             );
         """
-
-        c.execute(sql)
-        c.commit()
-        conn.close()
-        return True
-    except:
-        return False
+        return _simple_query_wrapper(db_name, sql, None)
 
 
 def _create_classes_table(db_name):
-    try:
-        conn = get_db_connection(db_name)
-        if not conn:
-            return False
-        c = conn.cursor()
-
         sql = """
             CREATE TABLE classes (
                 id INTEGER PRIMARY KEY,
                 class TEXT
             );
         """
-
-        c.execute(sql)
-        c.commit()
-        conn.close()
-        return True
-    except:
-        return False
+        return _simple_query_wrapper(db_name, sql, None)
 
 
 def _create_features_table(db_name):
-    try:
-        conn = get_db_connection(db_name)
-        if not conn:
-            return False
-        c = conn.cursor()
-
         sql = """
             CREATE TABLE features (
                 id INTEGER PRIMARY KEY,
                 feature TEXT
             );
         """
-
-        c.execute(sql)
-        c.commit()
-        conn.close()
-        return True
-    except:
-        return False
+        return _simple_query_wrapper(db_name, sql, None)
 
 
+def _create_training_datas_table(db_name, features):
+    sql = "CREATE TABLE training_datas (\
+            id INTEGER PRIMARY KEY,\
+            class TEXT\
+            %s);"
+    feature_sql = ""
+    for feature in features:
+        feature_sql += ", %s REAL"
+    sql = sql % feature_sql
+
+    return _simple_query_wrapper(db_name, sql, None)
 
 
+def _create_weights_table(db_name, features):
+    sql = "CREATE TABLE weights (\
+            id INTEGER PRIMARY KEY,\
+            class TEXT\
+            %s);"
+    feature_sql = ""
+    for feature in features:
+        feature_sql += ", %s REAL"
+    sql = sql % feature_sql
+
+    return _simple_query_wrapper(db_name, sql, None)
 
 
+def _create_historical_weights_table(db_name, features):
+    sql = "CREATE TABLE historical_weights (\
+            id INTEGER PRIMARY KEY,\
+            training_data_id INTEGER,\
+            class TEXT\
+            %s);"
+    feature_sql = ""
+    for feature in features:
+        feature_sql += ", %s REAL"
+    sql = sql % feature_sql
+
+    return _simple_query_wrapper(db_name, sql, None)
 
 
+def _create_classification_data_table(db_name, features):
+    sql = "CREATE TABLE classification_data (\
+            id INTEGER PRIMARY KEY,\
+            class TEXT\
+            %s);"
+    feature_sql = ""
+    for feature in features:
+        feature_sql += ", %s REAL"
+    sql = sql % feature_sql
+
+    return _simple_query_wrapper(db_name, sql, None)
 
 
+"""
+Insert Database Functions
+    - functions to insert data into the tables
+"""
 
 
+def _insert_perceptron_details(db_name, iterations):
+    if type(iterations) is int:
+        sql = "INSERT INTO perceptron_details (iterations) VALUES (?);"
+        args = (iterations, )
+        return _simple_query_wrapper(db_name, sql, args)
 
 
+def _insert_classes(db_name, classes):
+    sql = "INSERT INTO classes (class) VALUES (?)"
+    error_classes = []  # stores all error inserts
+    for k in classes:
+        if not _simple_query_wrapper(db_name, sql, (k, )):
+            error_classes.append(k)
+    if len(error_classes) > 0:
+        return False, error_classes
+    return True, None
+
+
+def _insert_features(db_name, features):
+    sql = "INSERT INTO features (feature) VALUES (?)"
+    error_classes = []  # stores all error inserts
+    for k in features:
+        if not _simple_query_wrapper(db_name, sql, (k, )):
+            error_classes.append(k)
+    if len(error_classes) > 0:
+        return False, error_classes
+    return True, None
+
+
+def _insert_training_data(db_name, klass, feature_dict):
+    sql = "INSERT INTO training_datas (class %s) VALUES (? %s);"
+    args = [klass]
+    feature_key_sql = ""
+    feature_arg_sql = ""
+
+    for k in feature_dict.keys():
+        args.append(feature_dict[k])
+        feature_arg_sql += ", ? "
+        feature_key_sql += ", %s" % k
+    sql = sql % (feature_key_sql, feature_arg_sql)
+    args = tuple(args)
+
+    return _simple_query_wrapper(db_name, sql, args)
+
+
+def _insert_weights(db_name, klass, feature_dict):
+    sql = "INSERT INTO weights (class %s) VALUES (? %s);"
+    args = [klass]
+    feature_key_sql = ""
+    feature_arg_sql = ""
+
+    for k in feature_dict.keys():
+        args.append(feature_dict[k])
+        feature_arg_sql += ", ? "
+        feature_key_sql += ", %s" % k
+    sql = sql % (feature_key_sql, feature_arg_sql)
+    args = tuple(args)
+
+    return _simple_query_wrapper(db_name, sql, args)
+
+
+def _insert_historical_weights(db_name, klass, training_id, feature_dict):
+    sql = "INSERT INTO historical_weights (class, training_data_id %s) VALUES (?, ? %s);"
+    args = [klass, int(training_id)]
+    feature_key_sql = ""
+    feature_arg_sql = ""
+
+    for k in feature_dict.keys():
+        args.append(feature_dict[k])
+        feature_arg_sql += ", ? "
+        feature_key_sql += ", %s" % k
+    sql = sql % (feature_key_sql, feature_arg_sql)
+    args = tuple(args)
+
+    return _simple_query_wrapper(db_name, sql, args)
+
+
+def _insert_classification_data(db_name, klass, feature_dict):
+    sql = "INSERT INTO classification_data (classified_class %s) VALUES (? %s);"
+    args = [klass]
+    feature_key_sql = ""
+    feature_arg_sql = ""
+
+    for k in feature_dict.keys():
+        args.append(feature_dict[k])
+        feature_arg_sql += ", ? "
+        feature_key_sql += ", %s" % k
+    sql = sql % (feature_key_sql, feature_arg_sql)
+    args = tuple(args)
+
+    return _simple_query_wrapper(db_name, sql, args)
+
+
+"""
+Select Database Functions
+    - functions to select data from tables
+"""
+
+def
 
 
 
